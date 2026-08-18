e3e09d6412fe8cf5b8ac587a49202e6a6412afbc
M3rv1nnn <mervinraj19@gmail.com>
2026-08-18 14:24:35 +0800

    Update Menu.h

--- Menu.h
+++ Menu.h
@@ -1,13 +1,15 @@
 #pragma once
+#include <iostream>
+#include <string> 
 
 class GameMenu {
 private:
     bool running;
 
     void openStates();
-    void openDialogueLog();
+    void openDialogueLog(const std::string log[], int count);
 
 public:
     GameMenu();
-    void display();
+    void display(const std::string log[], int count);
 };
